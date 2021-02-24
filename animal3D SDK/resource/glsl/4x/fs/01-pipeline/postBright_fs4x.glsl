219031dba60388d08b3fb9a1ba5260ac64fa97e9
Adel Talhouk <adel.talhouk@mymail.champlain.edu>
2021-02-23 19:19:06 -0500

    Saving so Charlie can test on his machine.

--- animal3D SDK/resource/glsl/4x/fs/01-pipeline/postBright_fs4x.glsl
+++ animal3D SDK/resource/glsl/4x/fs/01-pipeline/postBright_fs4x.glsl
@@ -34,6 +34,7 @@ layout (location = 0) out vec4 rtFragColor;
 layout (binding = 0) uniform sampler2D hdr_image;
 
 //Varyings
+//in vec2 vTexcoord;
 in vec2 vTexcoord_Atlas;
 
 void main()
@@ -42,17 +43,18 @@ void main()
 	float lum[25];
 	vec2 tex_scale = vec2(1.0) / textureSize(hdr_image, 0);
 
+	//Sample 25 texels centered on the current one (the sampler)
 	for (i = 0; i < 25; i++)
 	{
-		vec2 tc = (2.0 * gl_FragCoord.xy +
-		3.5 * vec2(i % 5 - 2, i / 5 - 2));
+		vec2 tc = (2.0 * vTexcoord_Atlas.xy +
+			3.5 * vec2(i % 5 - 2, i / 5 - 2));
 		vec3 col = texture(hdr_image, tc * tex_scale).rgb;
 		lum[i] = dot(col, vec3(0.3, 0.59, 0.11));
 	}
 
 	// Calculate weighted color of region
 	vec3 vColor = texelFetch(hdr_image,
-		2 * ivec2(gl_FragCoord.xy), 0).rgb;
+		2 * ivec2(vTexcoord_Atlas.xy), 0).rgb;
 
 	float kernelLuminance = (
 		(1.0 * (lum[0] + lum[4] + lum[20] + lum[24])) +
