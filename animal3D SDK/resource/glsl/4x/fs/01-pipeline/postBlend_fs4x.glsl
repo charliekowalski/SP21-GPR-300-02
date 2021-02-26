80206ec94312ccf6b2064ccaa9edc00dbf05d165
Adel Talhouk <adel.talhouk@mymail.champlain.edu>
2021-02-25 22:27:17 -0500

    Cleaned up code for psotBlend_fs4x.glsl. Marked it as done.

--- animal3D SDK/resource/glsl/4x/fs/01-pipeline/postBlend_fs4x.glsl
+++ animal3D SDK/resource/glsl/4x/fs/01-pipeline/postBlend_fs4x.glsl
@@ -24,7 +24,7 @@
 
 #version 450
 
-// ****TO-DO: --------------------------------------------------------------------------- Blue book p. 489 and Lecture 5: Bloom, slide 32
+// ****DONE: --------------------------------------------------------------------------- Blue book p. 489 and Lecture 5: Bloom, slide 32
 //	-> declare texture coordinate varying and set of input textures
 //	-> implement some sort of blending algorithm that highlights bright areas
 //		(hint: research some Photoshop blend modes)
@@ -42,36 +42,15 @@ in vec4 vTexcoord_atlas;
 
 void main()
 {
-	vec4 blend = vec4(0.0);
-	blend += texelFetch(sceneTexture, ivec2(gl_FragCoord.xy), 0);				//Use gl_FragCoord for the scene pass
-	blend += texelFetch(verticalBlur2, ivec2(vTexcoord_atlas.xy), 0);			//Use vTexcoord_atlas for postProcessing passes
-	blend += texelFetch(verticalBlur4, ivec2(vTexcoord_atlas.xy), 0);			//...
-	blend += texelFetch(verticalBlur8, ivec2(vTexcoord_atlas.xy), 0);			//...
-
-		//Use texture instead of textlFetch
-		//Use vTexcoord_atlas instgead of gl_FragCoord
-
+	//Blend the textures
 	vec4 testBlend = 1.0 - (1.0 - texture2D(sceneTexture, vTexcoord_atlas.xy))
-	* (1.0 - texture2D(verticalBlur2, vTexcoord_atlas.xy))
-	* (1.0 - texture2D(verticalBlur4, vTexcoord_atlas.xy))
-	* (1.0 - texture2D(verticalBlur8, vTexcoord_atlas.xy));
+		* (1.0 - texture2D(verticalBlur2, vTexcoord_atlas.xy))
+		* (1.0 - texture2D(verticalBlur4, vTexcoord_atlas.xy))
+		* (1.0 - texture2D(verticalBlur8, vTexcoord_atlas.xy));
 
-//	blend += texelFetch(verticalBlur8, ivec2(vTexcoord_atlas.xy), 0);			//Use vTexcoord_atlas for postProcessing passes
-//	blend += texelFetch(verticalBlur4, ivec2(vTexcoord_atlas.xy), 0);			//Use vTexcoord_atlas for postProcessing passes
-//	blend += texelFetch(verticalBlur2, ivec2(vTexcoord_atlas.xy), 0);			//Use vTexcoord_atlas for postProcessing passes
-//	blend += texelFetch(sceneTexture, ivec2(gl_FragCoord.xy), 0);				//Use gl_FragCoord for the scene pass
-
-//	vec4 testBlend = 1.0 - (1.0 - texelFetch(sceneTexture, ivec2(gl_FragCoord.xy), 0))
-//		* (1.0 - texelFetch(verticalBlur2, ivec2(vTexcoord_atlas.xy), 0))
-//		* (1.0 - texelFetch(verticalBlur4, ivec2(vTexcoord_atlas.xy), 0))
-//		* (1.0 - texelFetch(verticalBlur8, ivec2(vTexcoord_atlas.xy), 0));
-
-//	blend.rgb = vec3(1.0) - exp(-blend.rgb * 0.9);
-
-	//rtFragColor = 1.0 - blend;
-//	rtFragColor = blend;
+	//Assign the blended textures to the outp[ut colour
 	rtFragColor = testBlend;
 
 	// DUMMY OUTPUT: all fragments are OPAQUE PURPLE
-//	rtFragColor = vec4(0.5, 0.0, 1.0, 1.0);
+	//rtFragColor = vec4(0.5, 0.0, 1.0, 1.0);
 }
