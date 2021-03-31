5579310952b3f521500b5fad9b309e932884123e
Adel Talhouk <adel.talhouk@mymail.champlain.edu>
2021-03-31 18:59:57 -0400

    Saving. Working on drawVertexTangents(). Not working yet.

--- animal3D SDK/resource/glsl/4x/gs/00-common/drawTangentBasis_gs4x.glsl
+++ animal3D SDK/resource/glsl/4x/gs/00-common/drawTangentBasis_gs4x.glsl
@@ -83,7 +83,28 @@ void drawWireframe()
 	EndPrimitive();
 }
 
+void drawVertexTangents()
+{
+	//vTangentBasis_view[0] --> tangent,	vTangentBasis_view[1] --> bitangent,	vTangentBasis_view[2] --> normal
+
+	//Tangent
+	vec4 tangentAway = vVertexData[0].vTangentBasis_view[0];
+	gl_Position = gl_in[0].gl_Position;
+	gl_Position = gl_in[0].gl_Position + tangentAway;
+
+	//Bitangent
+	vec4 bitangentAway = vVertexData[0].vTangentBasis_view[1];
+	gl_Position = gl_in[1].gl_Position;
+	gl_Position = gl_in[1].gl_Position + bitangentAway;
+
+	//Normal
+	vec4 normalAway = vVertexData[0].vTangentBasis_view[2];
+	gl_Position = gl_in[2].gl_Position;
+	gl_Position = gl_in[2].gl_Position + normalAway;
+}
+
 void main()
 {
 	drawWireframe();
+	drawVertexTangents();
 }
