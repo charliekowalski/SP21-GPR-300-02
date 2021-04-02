0462f2423e72b36f757070d7eea65873ede58ee4
Adel Talhouk <adel.talhouk@mymail.champlain.edu>
2021-04-02 14:09:50 -0400

    Class work + taking notes.

--- animal3D SDK/resource/glsl/4x/vs/04-anim/passTangentBasis_morph_transform_vs4x.glsl
+++ animal3D SDK/resource/glsl/4x/vs/04-anim/passTangentBasis_morph_transform_vs4x.glsl
@@ -34,11 +34,22 @@
 //		(hint: results can be stored in local variables named after the 
 //		complete tangent basis attributes provided before any changes)
 
+/*
 layout (location = 0) in vec4 aPosition;
 layout (location = 2) in vec3 aNormal;
 layout (location = 8) in vec4 aTexcoord;
 layout (location = 10) in vec3 aTangent;
 layout (location = 11) in vec3 aBitangent;
+*/
+
+//Start attributes form scratch (attributes represent morph targets)
+//What is part of a single morph target? (see a3_DemoState-load.c around line 322)
+//	-> position, normal, tangent
+//	-> 16 available, 16 / 3 = 5 targets (int) + texcoord
+//What is NOT part of a single morph target?
+//	-> texcoord: shared because it is always the same in 2D, not a morphable attribute, does not change
+//	-> bitangent: cross(normal, tangent)
+//...
 
 struct sModelMatrixStack
 {
@@ -70,6 +81,13 @@ void main()
 {
 	// DUMMY OUTPUT: directly assign input position to output position
 	//gl_Position = aPosition;
+
+	//Results of morphing
+	vec4 aPosition;
+	vec3 aTangent, aBitangent, aNormal;
+
+	//Testing: copy the first morph target only --> will show teapot as if static (non-morphing but at least will be rendering)
+	//...
 	
 	sModelMatrixStack t = uModelMatrixStack[uIndex];
 	
